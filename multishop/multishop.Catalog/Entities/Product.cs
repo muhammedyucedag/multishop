da3e3405bb3b83afd392f97090d5833546844023
Muhammed Yücedağ <muhammedyucedagg@gmail.com>
2025-05-31 13:02:50 +0300

    product ve category sınıflarının oluşturulması.

---
+++ multishop/multishop.Catalog/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace multishop.Catalog.Entities;
+
+public class Product
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = null!;
+    public string Name { get; set; } = null!;
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string? ImageUrl { get; set; }
+    public string? Description { get; set; }
+}
